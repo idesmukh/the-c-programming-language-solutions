31be658b425817bb7e624b8bfd0fa7d7febeded9
idesmukh <ibaddesmukh@gmail.com>
2025-02-09 16:37:38 +0000

    solve: ex1-6

---
+++ chapter1/ex1-06_eof.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* verify that the expression getchar() != EOF is 0 or 1 */
+int main()
+{
+    int c;
+    
+    while (1) {
+        c = getchar();
+        printf("The expression getchar() != EOF evaluates to: %d\n", c!= EOF);
+        if (c == EOF) {
+            break;
+        }
+    }
+    return 0;
+}

6ca94c36aa1b0ec6a8e4b760b3ab10b76213aec2
idesmukh <ibaddesmukh@gmail.com>
2025-02-23 14:56:21 +0000

    Solve: ex1-8

--- chapter1/ex1-08_count.c
+++ chapter1/ex1-08_count.c
@@ -2,11 +2,24 @@
 
 /* Count blanks, tabs, and newlines in input */
 int main() {
-  int c, nl;
+  int c, blanks_count, tab_count, newline_count;
+  blanks_count = 0;
+  tab_count = 0;
+  newline_count = 0;
 
-  nl = 0;
-  while ((c = getchar()) != EOF)
-      if (c == '\n')
-          ++nl;
-  printf("%d\n", nl);
+  while ((c = getchar()) != EOF) {
+    if (c == ' ') {
+      ++blanks_count;
+    } else if (c == '\t') {
+      ++tab_count;
+    } else if (c == '\n') {
+      ++newline_count;
+    }
+  }
+  
+  printf("\nBlanks: %d\n", blanks_count);
+  printf("Tabs: %d\n", tab_count);
+  printf("Newlines: %d\n", newline_count);
+
+  return 0;
 }

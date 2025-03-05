ff1145914eeff3dc3dc6d219140e17e7b0952d6c
idesmukh <ibaddesmukh@gmail.com>
2025-03-05 17:34:18 +0000

    solve: ex1-11

--- chapter1/ex1-11_wc.c
+++ chapter1/ex1-11_wc.c
@@ -1,3 +1,10 @@
+/*
+Test 1: Empty input
+Test 2: Only lines
+Test 3: Only one word
+Test 4: Only one character
+Test 5: Leave some blanks and then input word
+*/
 #include <stdio.h>
 
 #define IN 1    /* inside a word */
